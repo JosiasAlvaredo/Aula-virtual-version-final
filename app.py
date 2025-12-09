fa345af8cdd14d5a6bb586bb2922b637c94e29da
Josías <d48325758@alumnos.itr.edu.ar>
2025-12-09 08:11:26 -0300

    iniciando

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
+        port=3304,
         password="root",
         database="aula_virtual"
     )
